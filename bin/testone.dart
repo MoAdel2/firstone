2be9742d58ff5928fdbd0b4af2017274a9d5124b
MoAdel2 <eng.adel1100@gmail.com>
2022-07-12 10:18:12 +0200

    first commit

---
+++ bin/testone.dart
@@ -0,0 +1,5 @@
+import 'package:testone/testone.dart' as testone;
+
+void main(List<String> arguments) {
+  print('Hello world: ${testone.calculate()}!');
+}
---
+++ test/testone_test.dart
@@ -0,0 +1,8 @@
+import 'package:testone/testone.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
